fcb7556b568b495628452774a61640662fa6238d
bbig <huangtengle@gmail.com>
2021-05-21 19:14:57 +0800

    move main to Main.java

--- java version/GDY/src/GDY/gandengyan.java
+++ java version/GDY/src/GDY/gandengyan.java
@@ -13,58 +13,6 @@ public class gandengyan
 	public static Pss Last_playing_card_type = new Pss(CardTypes[0], "-1");
 	public static ArrayList<Poker> Last_playing_card = new ArrayList<Poker>();
 	
-	public static void main(String[] args)
-	{
-		startGame();
-	    initGame();
-	    Shuffle();
-		
-		Player[] Players = new Player[Number_of_players];
-		for (int i = 0; i < Number_of_players; ++i)
-		{
-	        Players[i] = new Player(i, "Anonymous");
-		}
-		
-		for (int i = 0; i < 5; ++i)
-	        for (int j = 0; j < Number_of_players; ++j)
-	            Players[j].getPoker();
-	    Players[First_player].getPoker();
-	    
-//	    for (int i = 0; i < Number_of_players; ++i)
-//	    {
-//	    	System.out.println("Player " + i);
-//	        Players[i].display_card();
-//	        System.out.println();
-//	    }
-	    
-	    for (int i = First_player;; ++i)
-	    {
-	        i %= Number_of_players;
-	        clearScreen();
-	        if (Winner != -99)
-	        {
-	        	System.out.println("游戏结束");
-	            System.out.println("胜利者是玩家 " + Winner + "号");
-	            System.out.println("恭喜! !");
-	            break;
-	        }
-	        if (Last_playing_card.isEmpty() == false)
-	        {
-	        	System.out.println("上一轮出的牌是：");
-	        	for (var x : Last_playing_card)
-	            	System.out.print(x.getColor() + '|' + x.getPoint() + "  ");
-	            System.out.println();
-	        }
-	        Players[i].Round();
-	        if (Number_of_no == Number_of_players)
-	        {
-	            i--;
-	            Number_of_no = 0;
-	            Last_playing_card_type = new Pss(CardTypes[0], "-1");
-	        }
-	    }
-	}
-	
 	public static void clearScreen()
 	{
 	    //Clears Screen in java
