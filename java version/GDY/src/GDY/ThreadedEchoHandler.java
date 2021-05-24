0889992508f291e3cba810e4318fe7ef01067fff
bbig <huangtengle@gmail.com>
2021-05-24 14:05:42 +0800

    Update ThreadedEchoHandler.java

--- java version/GDY/src/GDY/ThreadedEchoHandler.java
+++ java version/GDY/src/GDY/ThreadedEchoHandler.java
@@ -7,11 +7,7 @@ import java.util.*;
 class ThreadedEchoHandler implements Runnable
 {
     private Socket incoming;
-
-    /**
-     Constructs a handler.
-     @param incomingSocket the incoming socket
-     */
+    
     public ThreadedEchoHandler(Socket incomingSocket)
     {
         incoming = incomingSocket;
@@ -21,11 +17,8 @@ class ThreadedEchoHandler implements Runnable
     {
         try (InputStream inStream = incoming.getInputStream();
              OutputStream outStream = incoming.getOutputStream();
-//             var in = new Scanner(inStream, StandardCharsets.UTF_8);
              var in = new Scanner(inStream, "UTF-8");
              var out = new PrintWriter(
-//                     new OutputStreamWriter(outStream, StandardCharsets.UTF_8),
-//                     true /* autoFlush */))
                      new OutputStreamWriter(outStream, "UTF-8"),
                      true /* autoFlush */))
         {
